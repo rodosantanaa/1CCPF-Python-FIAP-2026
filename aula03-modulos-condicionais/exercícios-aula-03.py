567a86c8dd64351a7e3ac5c0696fa03090f52709
rodosantanaa <rodrigozarzarsantana@gmaol.com>
2026-04-07 11:47:50 -0300

    exercicios da aula 03

---
+++ aula03-modulos-condicionais/exercícios-aula-03.py
@@ -0,0 +1,40 @@
+# 01
+import pygame
+pygame.init()
+pygame.mixer.music.load("esqueleto.mp3")
+pygame.mixer.music.play()
+input()
+pygame.event.wait()
+breakpoint()
+
+# 02
+# num = int(input("escolha um número: "))
+# if num %2 == 0:
+#     print("par")
+# else:
+#     print("ímpar")
+
+# 03
+# num_1 = int(input("escolha um número: "))
+# num_2 = int(input("escolha outro número: "))
+# if num_1 > num_2:
+#     print("o primeiro número é o maior")
+# elif num_1 < num_2:
+#     print("o segundo número é o maior")
+# elif num_1 == num_2:
+#     print("os números são iguais")
+
+# 04
+# nota_1 = int(input("digite a primeira nota: "))
+# nota_2 = int(input("digite a segunda nota: "))
+# nota_3 = int(input("digite a terceira nota: "))
+# nota_4 = int(input("digite a quarta nota: "))
+# media = (nota_1 + nota_2 + nota_3 + nota_4)/4
+# if media >= 7:
+#     print(f"aprovado! sua nota foi {media:.1f}")
+# elif media < 7 or media >= 5:
+#     print(f"recuperação. sua nota foi {media:.1f}")
+# elif media < 5:
+#     print(f"reprovado. sua nota foi {media:.1f}")
+
+# 05
